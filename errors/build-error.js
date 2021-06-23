09c3d6b6a4010f2b9bc6eeaae521289bbe6a8282
Jason Saelhof <jason@deepmarkit.com>
2021-06-22 23:57:41 -0600

    remove rest endpoints and rest error builders

--- errors/build-error.js
+++
@@ -1,1 +0,0 @@
-export const buildError = (errorCode) => ({errorCode});
--- pages/api/db/load.js
+++
@@ -1,7 +0,0 @@
-import {load} from "../../../db/db";
-
-const handler = (req, res) => {
-  res.status(200).json(load());
-};
-
-export default handler;
