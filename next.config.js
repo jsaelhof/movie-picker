335517b9be191ff00e8d8658875c07fafc848ef9
Jason Saelhof <jason@deepmarkit.com>
2021-08-10 23:13:15 -0600

    remove AUTH0_BASEURL from nextjs config and move it to .env.production file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ module.exports = {
   env: {
     GRAPHQL_URL: "/api/graphql",
     OMDB_API_URL: "http://www.omdbapi.com/",
-    AUTH0_BASE_URL: process.env.VERCEL_URL || process.env.AUTH0_BASE_URL,
   },
 };
