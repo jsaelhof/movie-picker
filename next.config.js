85ad46478a0ef88c3c6c22ead4a742252e20db75
Jason Saelhof <jsaelhof@gmail.com>
2021-09-18 12:09:13 -0600

    Switch to use PHASE_PRODUCTION_BUILD to detect prod deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const { PHASE_PRODUCTION_SERVER } = require("next/constants");
+const { PHASE_PRODUCTION_BUILD } = require("next/constants");
 
 module.exports = (phase) => ({
   env: {
     PHASE: phase,
-    IS_PROD: phase === PHASE_PRODUCTION_SERVER,
+    IS_PROD: phase === PHASE_PRODUCTION_BUILD,
     GRAPHQL_URL: "/api/graphql",
     OMDB_API_URL: "http://www.omdbapi.com/",
   },
