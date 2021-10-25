0adf6a29ba9cb1120fc612c9bd3974cfd6e625ab
Jason Saelhof <jsaelhof@gmail.com>
2021-10-25 00:02:26 -0600

    migrate list-header-cell to styled components

--- components/list-header-cell/list-header-cell.jsx
+++ components/list-header-cell/list-header-cell.jsx
@@ -1,21 +1,24 @@
-import clsx from "clsx";
-import noop from "lodash/noop";
 import React from "react";
+import { styled } from "@mui/material";
 
-import styles from "./list-header-cell.module.css";
-
-const ListHeaderCell = ({children, left, dense, onClick}) => (
-  <div
-    className={clsx(
-      styles.listHeader,
-      left && styles.left,
-      dense && styles.dense,
-      onClick && styles.pointer,
-    )}
-    onClick={onClick || noop}
-  >
-    {children}
-  </div>
+const ListHeaderCell = ({ children, ...props }) => (
+  <Cell {...props}>{children}</Cell>
 );
+
+const Cell = styled("div")(({ theme: { palette, spacing }, left }) => ({
+  display: "flex",
+  fontSize: "0.8em",
+  fontWeight: 700,
+  color: palette.darkBlue[500],
+  marginBottom: spacing(0.5),
+  borderBottom: "1px solid #eee",
+  padding: `${spacing(1)} ${spacing(2)} ${spacing(2)}`,
+  justifyContent: "center",
+  cursor: "default",
+
+  ...(left && {
+    justifyContent: "left",
+  }),
+}));
 
 export default ListHeaderCell;
