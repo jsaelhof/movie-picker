d5e1f48001fb3c605737eb981c9a3b88434d75a5
Jason Saelhof <jsaelhof@gmail.com>
2021-10-27 00:02:37 -0600

    migrate ratings to styled components

--- components/ratings/ratings.jsx
+++ components/ratings/ratings.jsx
@@ -1,32 +1,57 @@
-import styles from "./ratings.module.css";
-
 import React from "react";
-import clsx from "clsx";
 import has from "lodash/has";
 import map from "lodash/map";
+import { styled } from "@mui/material";
 
 import { ratingsSource, ratingsSourceImage } from "../../constants/ratings";
 
-const Ratings = ({ ratings, size = "medium", dense, className, ...props }) => (
-  <ul
-    {...props}
-    className={clsx(styles.ratings, dense && styles.dense, className)}
-  >
+const Ratings = ({ ratings, size = "medium", dense }) => (
+  <RatingsList $dense={dense}>
     {map(ratings, (rating, source) =>
       has(ratingsSource, source) && rating ? (
-        <li key={source}>
-          <img
+        <RatingsListItem key={source}>
+          <RatingsSourceIcon
+            $size={size}
             src={`/images/ratings/${ratingsSourceImage[source]}`}
-            className={clsx(
-              size === "medium" && styles.ratingsSourceIconMedium,
-              size === "small" && styles.ratingsSourceIconSmall
-            )}
           />
           {rating}
-        </li>
+        </RatingsListItem>
       ) : null
     )}
-  </ul>
+  </RatingsList>
+);
+
+const RatingsList = styled("ul")`
+  display: flex;
+  list-style-type: none;
+  padding-left: 0;
+  align-self: center;
+
+  ${({ $dense }) => $dense && { margin: 0 }}
+`;
+
+const RatingsListItem = styled("li")`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+const RatingsSourceIcon = styled("img")(({ $size }) =>
+  $size === "small"
+    ? {
+        width: 20,
+        height: 20,
+        marginRight: 8,
+      }
+    : {
+        width: 24,
+        height: 24,
+        marginRight: 8,
+      }
 );
 
 export default Ratings;
--- components/ratings/star-rating.jsx
+++ components/ratings/star-rating.jsx
@@ -1,13 +1,10 @@
-import styles from "./star-rating.module.css";
-
 import React, { useState } from "react";
-import clsx from "clsx";
 import { isNil, times } from "lodash";
 import { filter, flow, map, mean, round, thru } from "lodash/fp";
-import { Popover } from "@mui/material";
+import { Popover, styled } from "@mui/material";
 
 import { omitTypename } from "../../utils/omit-typename";
-import Star from "./star.svg";
+import StarFull from "./star.svg";
 import StarHalf from "./star-half.svg";
 import StarOutline from "./star-outline.svg";
 import Ratings from "./ratings";
@@ -21,13 +18,12 @@ const toAverageRating = flow(
   thru((avg) => avg / 2)
 );
 
-const StarRating = ({ ratings, anchor, className }) => {
+const StarRating = ({ ratings, anchor }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const avgRating = toAverageRating(omitTypename(ratings));
 
   return (
-    <div
-      className={clsx(styles.starRating, className)}
+    <StarRatingContainer
       onMouseEnter={(event) => setAnchorEl(event.currentTarget)}
       onMouseLeave={() => {
         setAnchorEl(null);
@@ -36,16 +32,15 @@ const StarRating = ({ ratings, anchor, className }) => {
       {times(5, (i) => {
         const offset = avgRating - i;
         if (offset >= 1) {
-          return <Star className={styles.star} key={i} />;
+          return <Star as={StarFull} key={i} />;
         } else if (offset === 0.5) {
-          return <StarHalf className={styles.star} key={i} />;
+          return <Star as={StarHalf} key={i} />;
         } else {
-          return <StarOutline className={styles.star} key={i} />;
+          return <Star as={StarOutline} key={i} />;
         }
       })}
 
-      <Popover
-        className={styles.popover}
+      <StyledPopover
         open={!!anchorEl}
         anchorEl={anchorEl}
         anchorOrigin={{
@@ -59,16 +54,39 @@ const StarRating = ({ ratings, anchor, className }) => {
         onClose={() => setAnchorEl(null)}
         disableRestoreFocus
         elevation={4}
-        classes={{
-          paper: styles.paper,
-        }}
       >
-        <div className={styles.tooltip}>
+        <Tooltip>
           <Ratings ratings={ratings} />
-        </div>
-      </Popover>
-    </div>
+        </Tooltip>
+      </StyledPopover>
+    </StarRatingContainer>
   );
 };
+
+const StarRatingContainer = styled("div")`
+  display: grid;
+  grid-auto-flow: column;
+  column-gap: 2px;
+  align-items: center;
+  width: 100px;
+`;
+
+const StyledPopover = styled(Popover)`
+  pointer-events: none;
+  margin-top: ${({ theme: { spacing } }) => spacing(1)};
+
+  & .MuiPopover-paper {
+    border-radius: 8px;
+  }
+`;
+
+const Tooltip = styled("div")`
+  padding: 0 ${({ theme: { spacing } }) => spacing(2)};
+`;
+
+const Star = styled("svg")`
+  height: 18.5px;
+  filter: drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.5));
+`;
 
 export default StarRating;
