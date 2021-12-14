9994bae68c3e4fad0bafe521a20a33829a1c9851
Jason Saelhof <jsaelhof@gmail.com>
2021-12-14 09:29:26 -0700

    add react-hooks plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
