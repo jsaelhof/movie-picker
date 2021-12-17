00e0f70441a1a201bd298390ab86e2d51b251380
Jason Saelhof <jsaelhof@gmail.com>
2021-12-17 09:51:34 -0700

    Upgrade version of mui. Remove styled components to use emotion instead. Tired of fighting with getting the override to work. Refactor titlebar styles.

--- components/titlebar/titlebar.jsx
+++ components/titlebar/titlebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, styled } from "@mui/material";
+import { AppBar, Box, Toolbar, useMediaQuery } from "@mui/material";
 import { useRouter } from "next/router";
 import Refresh from "@mui/icons-material/Refresh";
 
@@ -8,8 +8,12 @@ import ProfileMenu from "./profile-menu";
 import NavFull from "./nav-full";
 import Logo from "./logo";
 import NavHamburger from "./nav-hamburger";
-import NavButton from "./nav-button";
-import { useMediaQuery } from "@mui/material";
+import {
+  appBarContainerStyles,
+  PickAgainButton,
+  pickScreenToolbarStyles,
+  toolbarStyles,
+} from "./titlebar.styles";
 
 const TitleBar = () => {
   const { movies, setPick } = useAppContext();
@@ -20,9 +24,9 @@ const TitleBar = () => {
   const isPickScreen = pathname === "/pick";
 
   return (
-    <AppBarContainer>
+    <Box sx={appBarContainerStyles}>
       <AppBar position="static" color="transparent" elevation={2}>
-        <StyledToolbar sx={toolbarStyles(isPickScreen)}>
+        <Toolbar sx={[toolbarStyles, isPickScreen && pickScreenToolbarStyles]}>
           <Logo small={smallLogo} />
           {movies && (mobileNav ? <NavHamburger /> : <NavFull />)}
 
@@ -36,61 +40,10 @@ const TitleBar = () => {
             </PickAgainButton>
           )}
           <ProfileMenu />
-        </StyledToolbar>
+        </Toolbar>
       </AppBar>
-    </AppBarContainer>
+    </Box>
   );
 };
-
-const AppBarContainer = styled("div")(({ theme: { palette } }) => ({
-  flexGrow: 1,
-  zIndex: 1000,
-  background: `linear-gradient(75deg, ${palette.darkBlue[500]}, ${palette.darkBlue[900]} 80%)`,
-}));
-
-const StyledToolbar = styled(Toolbar)``;
-
-const toolbarStyles =
-  (isPickScreen) =>
-  ({ breakpoints }) => ({
-    display: "grid",
-    gridTemplateColumns: "auto 1fr auto",
-    gridTemplateAreas: `"logo nav profile"`,
-    minHeight: "64px",
-
-    [breakpoints.down(580)]: {
-      // "@media (max-width: 580px)": {
-      ...(isPickScreen
-        ? {
-            gridTemplateAreas: `"nav logo pick profile"`,
-            gridTemplateColumns: "auto max-content 1fr auto",
-          }
-        : { gridTemplateAreas: `"nav logo profile"` }),
-    },
-  });
-
-// const StyledToolbar = styled(Toolbar)(
-//   ({ theme: { breakpoints }, $isPickScreen }) => ({
-//     display: "grid",
-//     gridTemplateColumns: "auto 1fr auto",
-//     gridTemplateAreas: `"logo nav profile"`,
-//     minHeight: "64px",
-
-//     [breakpoints.down(580)]: {
-//       ...($isPickScreen
-//         ? {
-//             gridTemplateAreas: `"nav logo pick profile"`,
-//             gridTemplateColumns: "auto max-content 1fr auto",
-//           }
-//         : { gridTemplateAreas: `"nav logo profile"` }),
-//     },
-//   })
-// );
-
-const PickAgainButton = styled(NavButton)(({ theme: { spacing } }) => ({
-  gridArea: "pick",
-  marginRight: spacing(2),
-  marginLeft: "auto",
-}));
 
 export default TitleBar;
