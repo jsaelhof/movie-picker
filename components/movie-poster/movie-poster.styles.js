4d118dbaab8cb9a2a3f0f4591e7215264e182572
Jason Saelhof <jsaelhof@gmail.com>
2021-12-17 21:51:27 -0700

    migrate movie-poster to styled components

---
+++ components/movie-poster/movie-poster.styles.js
@@ -0,0 +1,33 @@
+import { styled } from "@mui/material";
+
+export const Poster = styled("div")`
+  background-color: white;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  overflow: hidden;
+  border-radius: 4px;
+  opacity: 1;
+`;
+
+export const active = {
+  cursor: "pointer",
+  "&:hover": {
+    transform: "scale(1.025)",
+  },
+};
+
+export const locked = {
+  opacty: 0.3,
+};
+
+export const NoPoster = styled(Poster)(({ palette }) => ({
+  display: "grid",
+  gridTemplateRows: "1fr 100px",
+  justifyItems: "center",
+  alignItems: "center",
+  textAlign: "center",
+  color: palette.grey[800],
+  fontSize: "0.9rem",
+  background: "#f7f7fc",
+}));
