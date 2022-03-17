5244906775b66c4f0b5b1c609a6c428ad8d02502
Jason Saelhof <jsaelhof@gmail.com>
2022-03-16 18:42:44 -0600

    update operation name for getRatings

--- graphql/queries/get-ratings.js
+++ graphql/queries/get-ratings.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const GET_RATINGS = gql`
-  query GetMovieDetails($imdbID: ID!) {
+  query GetRatings($imdbID: ID!) {
     omdbMovie(imdbID: $imdbID) {
       ratings {
         IMDB
