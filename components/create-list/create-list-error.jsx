e2bb21f02cd3e3ab44cf3810fcd87cc1ec121c47
Jason Saelhof <jsaelhof@gmail.com>
2022-03-29 23:59:20 -0600

    escape apostrophe

--- components/create-list/create-list-error.jsx
+++ components/create-list/create-list-error.jsx
@@ -5,7 +5,7 @@ import { Container } from "./create-list-error.styles";
 const CreateListError = ({ reset }) => {
   return (
     <Container>
-      <div>Sorry, we couldn't create the list.</div>
+      <div>Sorry, we couldn&apos;t create the list.</div>
       <div>
         <Button onClick={reset} variant="outlined" color="primary">
           Try Again
