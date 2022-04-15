1e1aa4a084f1a0e19c5d00134e8eee17187ce6fc
Jason Saelhof <jsaelhof@gmail.com>
2022-04-15 16:30:20 -0600

    refactor the pick util into a hook with history

--- pages/pick.js
+++ pages/pick.js
@@ -5,12 +5,71 @@ import { useAppContext } from "../context/app-context";
 import { errorMessage } from "../constants/error_codes";
 import ErrorDialog from "../components/error-dialog/error-dialog";
 import FullDetail from "../components/full-detail/full-detail";
-import { randomPick } from "../utils/random-pick";
+import { filter, conforms, sample, size } from "lodash";
+import { errorCodes } from "../constants/error_codes";
 
-export default function Home() {
+const useRandomPick = () => {
   const { movies, pick, setPick } = useAppContext();
   const { query } = useRouter();
-  const [error, setError] = useState(null);
+  const [history, setHistory] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    if (movies && !pick) {
+      const filters = {
+        locked: (locked) => !locked,
+      };
+
+      if (!query.minRuntime || !query.maxRuntime) {
+        filters.runtime = (runtime) =>
+          runtime >= (query.minRuntime || 0) &&
+          runtime <= (query.maxRuntime || Infinity);
+      }
+
+      const list = filter(movies, conforms(filters));
+
+      if (size(list) === 0) {
+        setError(errorCodes.PICKING);
+      } else {
+        // Recursive function to pick a movie that has not been picked before.
+        // Once every eligible movie has been picked, it will clear and start over.
+        const pickFunc = () => {
+          // Get a random movie.
+          const nextPick = sample(list);
+
+          // If the movie is in the history, call again.
+          if (history.includes(nextPick.id)) {
+            if (history.length === list.length) {
+              // Every movie is in the history already.
+              // Clear the history and start over.
+              // Keep the last movie as the first movie in the new history to prevent getting it again on the next pick.
+              setHistory(history.slice(-1));
+            } else {
+              // Try another movie.
+              pickFunc();
+            }
+          } else {
+            // This movie has not been seen in this rotation.
+            // Add it to the history and use it.
+            setHistory([...history, nextPick.id]);
+            setPick(nextPick);
+          }
+        };
+
+        pickFunc();
+      }
+    }
+  }, [history, movies, pick, query.maxRuntime, query.minRuntime, setPick]);
+
+  if (error) {
+    return { data: null, error };
+  } else {
+    return { data: pick, error: null };
+  }
+};
+
+export default function Home() {
+  const { pick, setPick } = useAppContext();
 
   // This is used to reset the pick in app state whne the page loads the first time.
   // Without this, when leaving the pick page and then picking again, the last pick will remain
@@ -18,33 +77,21 @@ export default function Home() {
   // eslint-disable-next-line
   useEffect(() => setPick(null), []);
 
-  useEffect(() => {
-    const pickMovie = () => {
-      try {
-        setPick(randomPick(movies, query));
-      } catch ({ message }) {
-        setError(message);
-      }
-    };
-
-    if (movies && !pick) {
-      pickMovie();
-    }
-  }, [movies, pick, query, setPick]);
-
-  if (!pick) return null;
+  const { error } = useRandomPick();
 
   return (
     <>
-      <FullDetail movie={pick} />
+      {pick && <FullDetail movie={pick} />}
 
-      <ErrorDialog
-        open={!!error}
-        content={
-          errorMessage[error] || errorMessage.UNKNOWN.replace("%%", error)
-        }
-        onConfirm={() => setError(null)}
-      />
+      {error && (
+        <ErrorDialog
+          open={!!error}
+          content={
+            errorMessage[error] || errorMessage.UNKNOWN.replace("%%", error)
+          }
+          onConfirm={() => setError(null)}
+        />
+      )}
     </>
   );
 }
