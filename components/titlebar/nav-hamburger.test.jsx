45f09af51fbca5765b47e95be943d7881c28cc00
Jason Saelhof <jsaelhof@gmail.com>
2022-07-29 21:26:09 -0600

    add more tests to nav-hamburger

--- components/titlebar/nav-hamburger.test.jsx
+++ components/titlebar/nav-hamburger.test.jsx
@@ -1,7 +1,8 @@
 import { renderWithProviders } from "../../utils/render-with-providers";
 import { useRouter } from "next/router";
-import { fireEvent, waitFor } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import NavHamburger from "./nav-hamburger";
+import { AppContext } from "../../context/app-context";
 
 jest.mock("next/router", () => ({
   useRouter: jest.fn(),
@@ -89,5 +90,53 @@ describe("nav-hamburger", () => {
     fireEvent.click(getByTestId("MenuIcon"));
 
     expect(getByRole("menuitem", { name: "Movies" })).toBeInTheDocument();
+  });
+
+  it("should not render the menu on the 'create' screen when there are no lists created yet", async () => {
+    useRouter.mockImplementation(() => ({
+      pathname: "/create",
+    }));
+
+    const { queryByTestId } = render(
+      <AppContext.Provider value={{ list: undefined }}>
+        <NavHamburger />
+      </AppContext.Provider>
+    );
+
+    await waitFor(() => {
+      expect(queryByTestId("MenuIcon")).not.toBeInTheDocument();
+    });
+  });
+
+  it("should close when clicking outside", async () => {
+    useRouter.mockImplementation(() => ({
+      pathname: "/",
+    }));
+
+    const { getByTestId, getByRole, queryByRole } = await renderWithProviders(
+      <NavHamburger />
+    );
+
+    await waitFor(() => {
+      expect(getByTestId("MenuIcon")).toBeInTheDocument();
+    });
+
+    fireEvent.click(getByTestId("MenuIcon"));
+
+    expect(getByRole("menuitem", { name: "Watched" })).toBeInTheDocument();
+
+    // Couldn't figure out a better way to make this work.
+    // There's some discussion here but seems the click away listener is not "armed" immediately due to a react bug.
+    // I've needed to use a wait here to make sure the click away is ready.
+    // https://github.com/mui/material-ui/issues/24783#issuecomment-774054038
+    await new Promise((r) => setTimeout(r, 1));
+
+    fireEvent.click(document);
+
+    await waitFor(() =>
+      expect(
+        queryByRole("menuitem", { name: "Watched" })
+      ).not.toBeInTheDocument()
+    );
   });
 });
